3b0318eccb1196926c2d6991430bc38848b2bc19
Trung Hà <trunghk.it@tripi.vn>
2019-08-27 13:18:01 +0700

    use input adornment

--- src/modules/common/components/SearchBox.tsx
+++ src/modules/common/components/SearchBox.tsx
@@ -9,7 +9,7 @@ import { LIGHT_GREY } from '../../../colors';
 import { ROUTES, SEARCH_INPUT } from '../../../constants';
 import closeSvg from '../../../svg/close.svg';
 import searchSvg from '../../../svg/search.svg';
-import { Line } from './elements';
+import { Line, BootstrapInput } from './elements';
 
 interface ISearchBoxProps extends InjectedIntlProps {
   dispatch: Dispatch;
@@ -30,40 +30,36 @@ const SearchBox: React.FunctionComponent<ISearchBoxProps> = props => {
   };
 
   return (
-    <Line
+    <BootstrapInput
       style={{
+        padding: '0 5px',
+        fontFamily: 'Open Sans',
+        fontWeight: 'bold',
+        fontSize: '14px',
+        lineHeight: '24px',
         width: '260px',
         height: '36px',
         borderRadius: '18px',
-        padding: '0 10px',
-        border: `1px solid ${LIGHT_GREY}`,
       }}
-    >
-      <IconButton size="small" onClick={search}>
-        <img alt="" src={searchSvg} width="20px" height="20px" />
-      </IconButton>
-      <InputBase
-        style={{
-          flex: 1,
-          padding: '0 5px',
-          fontFamily: 'Open Sans',
-          fontWeight: 'bold',
-          fontSize: '14px',
-          lineHeight: '24px',
-        }}
-        placeholder={intl.formatMessage({ id: 'searchAnything' })}
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        onKeyDown={e => {
-          if (e.keyCode === 13) {
-            search();
-          }
-        }}
-      />
-      <IconButton size="small" onClick={() => setInput('')}>
-        <img alt="" src={closeSvg} width="20px" height="20px" />
-      </IconButton>
-    </Line>
+      placeholder={intl.formatMessage({ id: 'searchAnything' })}
+      value={input}
+      onChange={e => setInput(e.target.value)}
+      onKeyDown={e => {
+        if (e.keyCode === 13) {
+          search();
+        }
+      }}
+      startAdornment={
+        <IconButton size="small" onClick={search}>
+          <img alt="" src={searchSvg} width="20px" height="20px" />
+        </IconButton>
+      }
+      endAdornment={
+        <IconButton size="small" onClick={() => setInput('')}>
+          <img alt="" src={closeSvg} width="20px" height="20px" />
+        </IconButton>
+      }
+    />
   );
 };
 
